64563a20639dbbcb8a5ad3e7f2a0da5a8e3e9c55
tiagomennab <tiago.menna@gmail.com>
2025-08-17 00:34:23 -0300

    ultimo

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -39,7 +39,7 @@ export const authOptions: NextAuthOptions = {
         return {
           id: user.id,
           email: user.email,
-          name: user.name,
+          name: user.name || undefined,
           plan: user.plan,
           creditsUsed: user.creditsUsed,
           creditsLimit: user.creditsLimit
